1098e2b6edb500ca038d42cb8286350422331253
anadolu-monster <ozandogulu47@gmail.com>
2023-11-05 12:23:30 +0300

    deneme1

---
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.searchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
+    String tiklanan_gun;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView =(SearchView) findViewById(R.id.search_view);
+        listView = (ListView) findViewById(R.id.list_view);
+        list = new ArrayList<>();
+        list.add("pazartesi");
+        list.add("salı");
+        list.add("çarşamba");
+        list.add("perşembe");
+        list.add("cuma");
+        list.add("cumartesi");
+        list.add("pazar");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if (b){
+                    listView.setVisibility(View.VISIBLE);
+                }else{
+                    listView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tiklanan_gun = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this,tiklanan_gun,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
